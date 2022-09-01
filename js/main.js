919c4cc220cce8e0852ef838d5038926231f8f9f
jasmine_p <jasmine.pfingsten@gmail.com>
2022-09-01 06:32:02 -0400

    Radio button changes, adding JS file

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Loop over radio inputs.
+// If one is checked, clear the checked property for all the others
+// Change checked button's label to have brighter gray background
+
+const radios = document.querySelectorAll('.btn-radio');
+
+radios.addEventListener('click', function (e) {
+  
+})
