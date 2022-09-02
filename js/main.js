7bfcf05fe42f7337255046d0c2c0fe94e907dfd2
jasmine_p <jasmine.pfingsten@gmail.com>
2022-09-02 06:34:17 -0400

    Finished initial style pass for mobile

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 // Loop over radio inputs.
 // If one is checked, clear the checked property for all the others
-// Change checked button's label to have brighter gray background
+// Change checked button's label to have brighter gray background (add .checked class)
 
 const radios = document.querySelectorAll('.btn-radio');
 
-radios.addEventListener('click', function (e) {
-  
-})
+// for (const radio of radios) {
+//   radio.addEventListener('click', function (e) {
+//     for (const radio of radios) {
+//       radio.checked = false;
+//     };
+//     e.target.classList.add('checked');
+//   })
+// }
