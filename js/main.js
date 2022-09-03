da20a68c2633d4f8c7cad64deb66d378d58c2ecb
jasmine_p <jasmine.pfingsten@gmail.com>
2022-09-03 19:18:30 -0400

    Begin adding JS

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const btnSubmit = document.querySelector('.btn-submit');
+const submittedRating = document.querySelector('#rating-value');
+
+
+btnSubmit.addEventListener('click', function () {
+
+})
