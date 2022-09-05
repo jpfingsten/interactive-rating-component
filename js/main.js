042e75d0decb21ca6cd90c899ec75d268b12c1d0
jasmine_p <jasmine.pfingsten@gmail.com>
2022-09-05 06:19:41 -0400

    Finished mobile version

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 const btnSubmit = document.querySelector('.btn-submit');
-const submittedRating = document.querySelector('#rating-value');
+const ratingCard = document.querySelector('#rating-card');
+const thankyouCard = document.querySelector('#thankyou-card');
+const submittedRatingText = document.querySelector('.submitted-rating');
 
 
 btnSubmit.addEventListener('click', function () {
+  const submittedRatingInput = document.querySelector('input[type=radio]:checked');
+  const submittedRatingValue = submittedRatingInput.value;
 
-})
+  submittedRatingText.innerText = `You selected ${submittedRatingValue} out of 5.`;
+  ratingCard.classList.add('hidden');
+  thankyouCard.classList.remove('hidden');
+});
